ac03da43ee90a38d6151c5f989a9696b8ed400e7
Rio6 <rio6.liu@gmail.com>
2019-12-03 21:47:54 -0800

    Use vector to calculate pitch

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+#include <MadgwickAHRS.h>
+#include <PrintEx.h>
+#include <vector_math.h>
+
+using namespace vmath;
+
+StreamEx eSerial = Serial;
+
 void setup() {
     Serial.begin(9600);
     Wire.begin();
@@ -17,38 +25,50 @@ void setup() {
     Wire.write(0x00); // Continuous measurement mode
     Wire.endTransmission();
 
+    // Joystick
+    pinMode(A2, INPUT);
+    pinMode(A3, INPUT);
+    pinMode(10, INPUT_PULLUP);
+
+    digitalWrite(3, LOW); // gnd
+    digitalWrite(4, HIGH); // v+
+
     pinMode(13, OUTPUT);
     pinMode(12, OUTPUT);
     pinMode(11, INPUT_PULLUP);
 }
 
 void loop() {
+    // MPU6050
     Wire.beginTransmission(0x68);
     Wire.write(0x3B);
     Wire.endTransmission(false);
-
     Wire.requestFrom(0x68, 14, true);
-    double accX = (Wire.read() << 8 | Wire.read()) / 16384.0;
-    double accY = (Wire.read() << 8 | Wire.read()) / 16384.0;
-    double accZ = (Wire.read() << 8 | Wire.read()) / 16384.0;
-    double temp = Wire.read() << 8 | Wire.read();
-    double gyroX = Wire.read() << 8 | Wire.read();
-    double gyroY = Wire.read() << 8 | Wire.read();
-    double gyroZ = Wire.read() << 8 | Wire.read();
 
+    vec3<double> acc;
+    acc.x = (Wire.read() << 8 | Wire.read()) / 16384.0;
+    acc.y = (Wire.read() << 8 | Wire.read()) / 16384.0;
+    acc.z = (Wire.read() << 8 | Wire.read()) / 16384.0;
+
+    double temp = ((Wire.read() << 8 | Wire.read()) + 521) / 340.0;
+
+    vec3<double> gyro;
+    gyro.x = (Wire.read() << 8 | Wire.read()) / 1090.0;
+    gyro.y = (Wire.read() << 8 | Wire.read()) / 1090.0;
+    gyro.z = (Wire.read() << 8 | Wire.read()) / 1090.0;
+
+    // HMC5883
     Wire.beginTransmission(0x1E);
     Wire.write(0x03);
     Wire.endTransmission();
-
     Wire.requestFrom(0x1E, 6, true);
-    double  magX = Wire.read() << 8 | Wire.read();
-    double  magZ = Wire.read() << 8 | Wire.read();
-    double  magY = Wire.read() << 8 | Wire.read();
 
-    double pitch = atan2(accY, sqrt(accX*accX + accZ*accZ));
-    double heading = atan2(magX * accZ - magZ * accY, magY); // The board is sideways
+    vec3<double> mag;
+    mag.y = (Wire.read() << 8 | Wire.read()) / 1370.0; // The board is rotated 90 deg
+    mag.z = (Wire.read() << 8 | Wire.read()) / 1370.0;
+    mag.x =-(Wire.read() << 8 | Wire.read()) / 1370.0; // so swap x y, invert x
 
-    Serial.print(pitch); Serial.print(" "); Serial.println(heading);
+    double alt = asin(dot(acc, vec3<double> {0, 1, 0}) / length(acc));
 
     /*
     static char cmd0 = 0, cmd1 = 0;
