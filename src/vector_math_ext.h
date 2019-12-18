1fca578c7e3dd66d792ac7221f1ebf60230ac8b3
Rio6 <rio6.liu@gmail.com>
2019-12-18 10:21:48 -0800

    Quaternion functions WIP

--- src/vector_math_ext.h
+++ src/vector_math_ext.h
@@ -8,31 +8,29 @@ namespace vmath {
         return (num > 0) - (num < 0);
     }
 
-    template<typename T>
+    template <typename T>
     inline vec3<T> project_to_plane(vec3<T> a, vec3<T> norm) {
         return a - dot(a, norm) * norm;
     }
 
-    template<typename T>
+    template <typename T>
     inline T angle_between(vec3<T> a, vec3<T> b) {
-        double s = sign(cross(a, b).z); // signess of the angle
+        double s = sign(cross(a, b).z); // TODO get sign of the angle properly
         return s * acos(dot(a, b));
     }
 
-    template<typename T>
-    inline quat<T> euler_to_quat(T yaw, T pitch, T roll) {
-        T cy = cos(yaw / T(2));
-        T sy = sin(yaw / T(2));
-        T cp = cos(pitch / T(2));
-        T sp = sin(pitch / T(2));
-        T cr = cos(roll / T(2));
-        T sr = sin(roll / T(2));
+    template <typename T>
+    inline quat<T> gyro_to_quat(vec3<T> v) {
+        T th = length(v);
+        v /= th; // normalize
+        T s = sin(th/2), c = cos(th/2);
+        return quat<T> (s * v.x, s * v.y, s * v.z, c);
+    }
 
-        return quat<T> {
-            cy * cp * sr - sy * sp * cr,
-            sy * cp * sr + cy * sp * cr,
-            sy * cp * cr - cy * sp * sr,
-            cy * cp * cr + sy * sp * sr
-        };
+    template <typename T>
+    inline T project_quat(quat<T> rot, vec3<T> v) {
+        auto prep = cross(v, rot.v); // a vector prependicular to v
+        auto rotated = quat_to_mat3(rot) * prep;
+        return angle_between(project_to_plane(rotated, v), prep);
     }
 };
