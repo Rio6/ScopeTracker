9a53ae091712fdc478c7f1022da31789a1fcc0d3
Rio6 <rio6.liu@gmail.com>
2020-03-30 12:38:19 -0700

    Move rust source out

--- tools/env.sh
+++ tools/env.sh
@@ -5,7 +5,7 @@ start_xargo_env() {
     basedir=$(dirname $(cargo locate-project | jq -r .root))
     export PATH="$PATH:$HOME/.platformio/packages/toolchain-atmelavr/bin/:$HOME/.cargo/bin:$basedir/tools"
     export AVRDUDE_PATH="$HOME/.platformio/packages/tool-avrdude"
-    export XARGO_RUST_SRC="$basedir/rust/src"
+    export XARGO_RUST_SRC="$HOME/.rustup/toolchains/avr-toolchain/lib/rustlib/src/rust/src"
     export RUST_TARGET_PATH="$basedir"
     export RUSTUP_TOOLCHAIN="avr-toolchain"
     export PS1="$(sed 's/\(.*\)\$\(.*\)/\1 [xargo-env]$\2/' <<< "$PS1")"
