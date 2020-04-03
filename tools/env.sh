e06f45d4d684e198227f74830adb5dfa57e90b67
Rio6 <rio6.liu@gmail.com>
2020-04-02 20:22:10 -0700

    Update env setup script

--- tools/env.sh
+++ tools/env.sh
@@ -4,11 +4,11 @@ start_xargo_env() {
 
     basedir=$(dirname $(cargo locate-project | jq -r .root))
     export PATH="$PATH:$HOME/.platformio/packages/toolchain-atmelavr/bin/:$HOME/.cargo/bin:$basedir/tools"
+    export PS1="$(sed 's/\(.*\)\$\(.*\)/\1 [xargo-env]$\2/' <<< "$PS1")"
     export AVRDUDE_PATH="$HOME/.platformio/packages/tool-avrdude"
     export XARGO_RUST_SRC="$HOME/.rustup/toolchains/avr-toolchain/lib/rustlib/src/rust/src"
     export RUST_TARGET_PATH="$basedir"
     export RUSTUP_TOOLCHAIN="avr-toolchain"
-    export PS1="$(sed 's/\(.*\)\$\(.*\)/\1 [xargo-env]$\2/' <<< "$PS1")"
     export XARGO_ENV_SET=1
 
     echo "xargo environment activated"
@@ -19,6 +19,7 @@ stop_xargo_env() {
     PATH=$OLD_PATH
     PS1=$OLD_PS1
 
+    unset AVRDUDE_PATH
     unset XARGO_RUST_SRC
     unset RUST_TARGET_PATH
     unset RUSTUP_TOOLCHAIN
@@ -29,7 +30,6 @@ stop_xargo_env() {
 
 if [ -z "$XARGO_ENV_SET" ]; then
     start_xargo_env
-    trap stop_xargo_env EXIT
 else
     stop_xargo_env
 fi
